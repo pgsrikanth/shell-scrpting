60fd49daef80ecf1a351dbb2d9c71296e4730ad0
srikanth <gowtham@1996.com>
2023-06-07 17:36:04 +0530

    about special variables

--- 07.special-variables.sh
+++ 07.special-variables.sh
@@ -1,6 +1,6 @@
-echo $0 # script name
-echo $1 # frist argument
-echo $2 # second argument
-echo $* # all arguments
-echo $@ # all arguments
-echo $# # No of arguments
+echo $0 ## script name
+echo $1 ## frist argument
+echo $2 ## second argument
+echo $* ## all arguments
+echo $@ ## all arguments
+echo $# ## No of arguments
