13d5bfdbccc6ada160c0fc6baf7baefd4140a0f6
srikanth <gowtham@1996.com>
2023-06-07 17:23:30 +0530

    about variables concept

--- 05.variables.sh
+++ 05.variables.sh
@@ -12,3 +12,10 @@ echo ouput is $a
 
 DATE=$(date)
 echo today date is $DATE
+
+#readonly varible
+readonly a1=10 (if anyone try to change the a1 value, that is not passible)
+
+#scalar
+   a1= (10 20 30)
+   echo ${a(0)} it gives 10
