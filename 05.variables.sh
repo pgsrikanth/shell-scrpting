7a6e1a2901ff6cfec2f158d83d29862aa4af322d
srikanth <gowtham@1996.com>
2023-06-07 17:02:56 +0530

    about variables concept

---
+++ 05.variables.sh
@@ -0,0 +1,14 @@
+# if you asign a name to set of data that is called variable
+# variables names have only charactors A-Z, a-z, 0-9, _
+# variables should not start with numbers
+
+
+#examples:
+#in script
+a=10
+echo $a
+ouput is 10
+
+
+DATE=$(date)
+echo today date is $DATE
